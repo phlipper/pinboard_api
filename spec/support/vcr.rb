e23c37e15bdfdfbaecbb975b2df0c74871e6e2b7
Phil Cohen <github@phlippers.net>
2012-07-14 01:02:22 -0700

    move and re-record vcr cassettes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,7 @@ require "base64"
 VCR.configure do |c|
   c.default_cassette_options = { record: :once, serialize_with: :json }
   c.hook_into :faraday
-  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  c.cassette_library_dir = "spec/vcr_cassettes"
   c.filter_sensitive_data("[USERNAME]")  { PinboardApi.username }
   c.filter_sensitive_data("[PASSWORD]")  { PinboardApi.password }
   c.filter_sensitive_data("[AUTHTOKEN]") { PinboardApi.auth_token }
